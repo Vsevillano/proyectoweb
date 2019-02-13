3285a975b18f2c1bfd5a3bd26a69a1e4730e997d
Victoriano Sevillano Vega <victorianosevillano@gmail.com>
2019-02-13 12:35:20 +0100

    Prueba

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Input;
-use App\Models\Post;
+use App\Post;
 
 
 class PostController extends Controller
