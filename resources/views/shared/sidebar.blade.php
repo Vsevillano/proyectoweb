a9648abdebd5306c564cdbcb9aec03ec1ee32c8c
Victoriano Sevillano Vega <victorianosevillano@gmail.com>
2019-02-20 10:13:07 +0100

    Máscambios

---
+++ resources/views/shared/sidebar.blade.php
@@ -0,0 +1,87 @@
+<!-- Sidebar Widgets Column -->
+<div class="col-md-4">
+
+  <!-- Search Widget -->
+  <div class="card mb-4">
+    <h5 class="card-header">Buscar</h5>
+    <div class="card-body">
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Buscar...">
+        <span class="input-group-btn">
+          <button class="btn btn-secondary" type="button">Ir</button>
+        </span>
+      </div>
+    </div>
+  </div>
+
+  <!-- Categories Widget -->
+  <div class="card my-4">
+    <h5 class="card-header">Categorias</h5>
+    <div class="card-body">
+      <div class="row">
+        <div class="col-lg-6">
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#">Desarrollo web</a>
+            </li>
+            <li>
+              <a href="#">HTML</a>
+            </li>
+            <li>
+              <a href="#">CSS</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-6">
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#">JavaScript</a>
+            </li>
+            <li>
+              <a href="#">Laravel</a>
+            </li>
+            <li>
+              <a href="#">Bootstrap</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Side Widget -->
+  <div class="card my-4">
+    <h5 class="card-header">Etiquetas</h5>
+    <div class="card-body">
+      <div class="row">
+        <div class="col-lg-6">
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#">Servicios</a>
+            </li>
+            <li>
+              <a href="#">Java</a>
+            </li>
+            <li>
+              <a href="#">CSS</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-6">
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#">HTML5</a>
+            </li>
+            <li>
+              <a href="#">Docker</a>
+            </li>
+            <li>
+              <a href="#">kubernetes</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
