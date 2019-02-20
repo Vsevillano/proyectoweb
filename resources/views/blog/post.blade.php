98b44c0b8b32767f64822510b749f99c71925eeb
Victoriano Sevillano Vega <victorianosevillano@gmail.com>
2019-02-20 10:44:29 +0100

    Algunos cambios mas

--- resources/views/blog/post.blade.php
+++ resources/views/blog/post.blade.php
@@ -40,22 +40,6 @@
 
           <hr>
 
-          <!-- Comments Form -->
-          <div class="card my-4">
-            <h5 class="card-header">Deja un comentario:</h5>
-            <div class="card-body">
-                <form action="{{ url('/blog/post/postCreatecomment') }}" method="POST">
-                  {!! csrf_field() !!}
-                  <input type="hidden" name="post_id" value="{{$post->id}}">
-                  <input type="hidden" name="user" value="1">
-
-                <div class="form-group">
-                  <textarea class="form-control" name="comment" rows="3"></textarea>
-                </div>
-                <button type="submit" class="btn btn-primary">Comentar</button>
-              </form>
-            </div>
-          </div>
           @foreach($comments as $comment)
           <!-- Single Comment -->
           <div class="media mb-4">
@@ -67,96 +51,35 @@
           </div>
           @endforeach
 
+        
+          @if (Auth::guest())
+          <div class="media mb-4">
+              <p><strong>Por favor, inicie sesión en el sistema para comentar.</strong></p>
+          </div>
 
+          @else
+          <!-- Comments Form -->
+          <div class="card my-4">
+            <h5 class="card-header">Deja un comentario:</h5>
+            <div class="card-body">
+                <form action="{{ url('/blog/post/postCreatecomment') }}" method="POST">
+                  {!! csrf_field() !!}
+                  <input type="hidden" name="post_id" value="{{$post->id}}">
+                  <input type="hidden" name="user" value="{{ auth()->user()->name }}">
+
+                <div class="form-group">
+                  <textarea class="form-control" name="comment" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Comentar</button>
+              </form>
+            </div>
+          </div>
+          @endif
 
         </div>
+        <!-- /.col-lg-8 -->
 
-		<!-- Sidebar Widgets Column -->
-        <div class="col-md-4">
-
-          <!-- Search Widget -->
-          <div class="card mb-4">
-            <h5 class="card-header">Buscar</h5>
-            <div class="card-body">
-              <div class="input-group">
-                <input type="text" class="form-control" placeholder="Buscar...">
-                <span class="input-group-btn">
-                  <button class="btn btn-secondary" type="button">Ir</button>
-                </span>
-              </div>
-            </div>
-          </div>
-
-          <!-- Categories Widget -->
-          <div class="card my-4">
-            <h5 class="card-header">Categorias</h5>
-            <div class="card-body">
-              <div class="row">
-                <div class="col-lg-6">
-                  <ul class="list-unstyled mb-0">
-                    <li>
-                      <a href="#">Desarrollo web</a>
-                    </li>
-                    <li>
-                      <a href="#">HTML</a>
-                    </li>
-                    <li>
-                      <a href="#">CSS</a>
-                    </li>
-                  </ul>
-                </div>
-                <div class="col-lg-6">
-                  <ul class="list-unstyled mb-0">
-                    <li>
-                      <a href="#">JavaScript</a>
-                    </li>
-                    <li>
-                      <a href="#">Laravel</a>
-                    </li>
-                    <li>
-                      <a href="#">Bootstrap</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <!-- Side Widget -->
-          <div class="card my-4">
-            <h5 class="card-header">Etiquetas</h5>
-            <div class="card-body">
-              <div class="row">
-                <div class="col-lg-6">
-                  <ul class="list-unstyled mb-0">
-                    <li>
-                      <a href="#">Servicios</a>
-                    </li>
-                    <li>
-                      <a href="#">Java</a>
-                    </li>
-                    <li>
-                      <a href="#">CSS</a>
-                    </li>
-                  </ul>
-                </div>
-                <div class="col-lg-6">
-                  <ul class="list-unstyled mb-0">
-                    <li>
-                      <a href="#">HTML5</a>
-                    </li>
-                    <li>
-                      <a href="#">Docker</a>
-                    </li>
-                    <li>
-                      <a href="#">kubernetes</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-		</div>
+        @include('shared.sidebar')
 
       </div>
       <!-- /.row -->
